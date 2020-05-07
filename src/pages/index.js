ec93dfe3abced3025d4f6a75f43bc429cda9f675
philhawker <philhawker1211@gmail.com>
2020-05-07 02:13:49 -0600

    hid scrollbar across app

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
-import Video from "../components/video"
 
 
 class IndexPage extends React.Component {
@@ -93,9 +92,9 @@ class IndexPage extends React.Component {
 
   render() {
     return (
-      <Layout location={this.props.location}>
+      <Layout location={this.props.location} >
         <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
-          <div id="wrapper">
+          <div id="wrapper"  >
             <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
             <Main
               isArticleVisible={this.state.isArticleVisible}
@@ -104,17 +103,18 @@ class IndexPage extends React.Component {
               article={this.state.article}
               onCloseArticle={this.handleCloseArticle}
               setWrapperRef={this.setWrapperRef}
+
             />
             <Footer timeout={this.state.timeout} />
           </div>
 
           {/* uncomment this to use static background image instead of video */}
 
-          {/* <div id="bg" /> */}
+          <div id="bg" />
 
-          <div id="video">
+          {/* <div id="video">
             <Video />
-          </div>
+          </div> */}
         </div>
       </Layout >
     )
