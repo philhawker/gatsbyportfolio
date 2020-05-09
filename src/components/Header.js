d48c92bb643dffa85a4ba3903d37f781507c607c
philhawker <philhawker1211@gmail.com>
2020-05-08 21:53:00 -0600

    added logo. removed generic diamond logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import ph from "../images/logos/ph.png"
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}} >
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon">
+
+      </span>
     </div>
     <div className="content">
       <div className="inner">
