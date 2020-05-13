0ab576588ff05ef4f45ac630461bfbc3c560e4e1
philhawker <philhawker1211@gmail.com>
2020-05-12 20:14:42 -0600

    wired backend logic to contact form to allow messages to be send by users

--- src/components/Main.js
+++ src/components/Main.js
@@ -297,14 +297,15 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact Me</h2>
-          <form method="post" action="#">
+          <form method="post" action="https://formspree.io/philhawker1211@gmail.com">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="_replyto" id="email" />
+
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
